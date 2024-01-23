8bcc105c71012854bc0cdc3f791a1460e7e1ef45
PatoProgramador <skpates18@gmail.com>
2024-01-23 17:56:09 -0500

    Solve functions optional activity

---
+++ html&js/funcionesJS/primerReto.js
@@ -0,0 +1,28 @@
+// 1
+function greeting() {
+    console.log('Hola mundo');
+}
+// 2
+function greetingWName(name) {
+    console.log(`¡Hola, ${name}`);
+}
+// 3
+function doubleNumber(number) {
+    return parseInt(number*2);
+}
+// 4
+function calculateAverage(number1, number2, number3) {
+    return (number1 + number2 + number3)/ 3;
+}
+// 5
+function maxNumber(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+// 6
+function multiplyNumber(number) {
+    return number * number;
+}
